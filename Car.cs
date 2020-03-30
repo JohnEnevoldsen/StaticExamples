c324e301fd9257706c72d4313f9ce68fc575d6ab
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2020-03-30 13:49:57 +0200

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace StaticExamples
+{
+    /// <summary>
+    /// A very simple representation of a car
+    /// </summary>
+    public class Car
+    {
+        #region Instance fields
+        private string _licensePlate;
+        private int _price;
+        private static int _numberOfCars;
+        private static int _usesOfLicensePlate;
+        private static int _usesOfPrice;
+        #endregion
+
+        #region Constructor
+        public Car(string licensePlate, int price)
+        {
+            _licensePlate = licensePlate;
+            _price = price;
+            _numberOfCars++;
+        }
+        #endregion
+
+        #region Properties
+        public string LicensePlate
+        {
+            get { _usesOfLicensePlate++; return _licensePlate; }
+            set { _licensePlate = value; _usesOfLicensePlate++;}
+        }
+
+        public int Price
+        {
+            get { _usesOfPrice++; return _price; }
+            set { _price = value; _usesOfPrice++;}
+        } 
+        #endregion
+        public static void PrintUsageStatistics(){
+            Console.WriteLine($"There are a total of {_numberOfCars} car(s)");
+            Console.WriteLine($"The property LicensePlate have been used {_usesOfLicensePlate} time(s)");
+            Console.WriteLine($"The property Price have been used {_usesOfPrice} time(s)");
+        }
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StaticExamples
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            #region ListMethods test
+            List<int> aList = new List<int> { 23, 86, 51, 11, 39 };
+
+            int smallest = ListMethods.FindSmallestNumber(aList);
+            Console.WriteLine($"The smallest number in the list is : {smallest}");
+
+            int average = ListMethods.FindAverage(aList);
+            Console.WriteLine($"The average of the list is : {average}"); 
+            #endregion
+
+            Car hatchBack = new Car("3EME926", 5000);
+            Car sedan = new Car("XOS 846", 6969);
+            Car mPV = new Car("JX 9817" , 4000);
+            Car sUV = new Car("8DR 592", 8000);
+
+            hatchBack.Price = 300000;
+            sedan.Price = 200000;
+            Console.WriteLine(hatchBack.Price);
+            Console.WriteLine(hatchBack.LicensePlate);
+            Console.WriteLine(mPV.LicensePlate);
+
+            Car.PrintUsageStatistics();
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
